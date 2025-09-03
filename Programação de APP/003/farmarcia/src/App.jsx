a486893852e292f042f153e1c0c195cfd9fdebb2
Alioliv <alyceoliveira0717@gmail.com>
2025-09-02 22:30:05 -0300

    atividade

---
+++ Programação de APP/003/farmarcia/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Corpo from './components/Corpo'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <Navbar />
+      <Home />
+    </div>
+  )
+}
+
+export default App
